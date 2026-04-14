ff4b488d4db74d6abe6045e8a2f860c01a0b76c0
Bruce Kuo <bruce800606@gmail.com>
2026-04-14 13:47:21 +0800

    Update pool.js

--- volvo-upload-test/db/pool.js
+++ volvo-upload-test/db/pool.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: process.env.POSTGRES_CONNECTION_STRING || process.env.DATABASE_URL,
+  connectionString: process.env.POSTGRES_CONNECTION_STRING,
   ssl: false,
   max: 10,
 });
